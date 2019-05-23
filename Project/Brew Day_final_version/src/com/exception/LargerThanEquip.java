26247ddc729bc601ed87f6039eda23099e046cca
e <l>
2019-05-23 23:29:03 +0800

    Final update version for Brew code

--- Project/Brew Day_final_version/src/com/exception/LargerThanEquip.java
+++
@@ -1,8 +0,0 @@
-package com.exception;
-
-public class LargerThanEquip extends Exception {
-
-	public LargerThanEquip() {
-		System.out.println("The batch size is larger than the equipment capacity");
-	}
-}
--- Project/Brew Day_final_version/src/com/listener/ModelListener.java
+++
@@ -1,5 +0,0 @@
-package com.listener;
-
-public interface ModelListener {
-	public void update();		// Update the corresponding data
-}
